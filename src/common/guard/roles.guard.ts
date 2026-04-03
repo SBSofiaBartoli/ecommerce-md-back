27a986dc63662625ff2a0a7a1ba9a8181a95b1b6
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-02 23:22:05 -0300

    refactor(roles-guard): uso constante ROLES_KEY importada del decorador

--- src/common/guard/roles.guard.ts
+++ src/common/guard/roles.guard.ts
@@ -2,13 +2,14 @@ import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@
 import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
 import { Rol } from "../enum/roles.enum";
+import { ROLES_KEY } from '../decorator/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
     constructor (private readonly reflector: Reflector) {}
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const requiredRoles = this.reflector.getAllAndOverride<Rol[]>('roles', [
+        const requiredRoles = this.reflector.getAllAndOverride<Rol[]>(ROLES_KEY, [
             context.getHandler(),
             context.getClass(),
         ]);
