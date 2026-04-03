0ce10afc34b3efd4af3fb96562991f55d54cade5
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-02 23:17:21 -0300

    refactor(roles): exportar constante ROLES_KEY para desacoplar decorador y guard

--- src/common/decorator/roles.decorator.ts
+++ src/common/decorator/roles.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from "@nestjs/common";
 import { Rol } from "src/common/enum/roles.enum";
 
-export const Roles = (...roles: Rol[]) => SetMetadata('roles', roles);
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Rol[]) => SetMetadata(ROLES_KEY, roles);
