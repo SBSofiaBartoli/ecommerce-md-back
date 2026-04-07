1a14cf7dd6c9308b4124dc29966ce77935994727
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-07 20:02:41 -0300

    feat(categories): agregar campo description a la entidad category

--- src/categories/entities/categories.entity.ts
+++ src/categories/entities/categories.entity.ts
@@ -2,15 +2,18 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from "../../products/entities/products.entity";
 
 @Entity({
-    name: 'categories',
+  name: "categories",
 })
 export class Category {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({ type: 'varchar', length: 50, nullable: false, unique: true, })
-    name: string;
+  @Column({ type: "varchar", length: 50, nullable: false, unique: true })
+  name: string;
 
-    @OneToMany(() => Product, (product) => product.category)
-    products: Product[];
+  @Column({ type: "text", nullable: true })
+  description: string;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products: Product[];
 }
