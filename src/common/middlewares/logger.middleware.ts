13082098935a1bca567726f6b6ede385ce2c2bda
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-07 19:50:25 -0300

    refactor(logger): simplificar log a formato ISO timestamp + metodo + URL

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -3,9 +3,9 @@ import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: NextFunction) {
-        const date = new Date().toISOString();
-        console.log(`Running method ${req.method} on url ${req.originalUrl} at this time ${date}`);
-        next();
-    }
+  use(req: Request, res: Response, next: NextFunction): void {
+    const timestamp = new Date().toISOString();
+    console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
+    next();
+  }
 }
