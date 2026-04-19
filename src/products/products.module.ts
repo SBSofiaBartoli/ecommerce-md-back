00b419cbcc99cc956cafadb1d0048cacd2e2480b
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-19 19:21:44 -0300

    feat(products/module): registrar ProductColor en TypeORM y exportar servicio

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,11 +4,12 @@ import { ProductsService } from "./products.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Product } from "./entities/products.entity";
 import { Category } from "src/categories/entities/categories.entity";
+import { ProductColor } from "./entities/product-color.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Product, Category])],
-    controllers: [ProductsController],
-    providers: [ProductsService],
-    exports: [ProductsService]
+  imports: [TypeOrmModule.forFeature([Product, ProductColor, Category])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
+  exports: [TypeOrmModule, ProductsService],
 })
 export class ProductsModule {}
