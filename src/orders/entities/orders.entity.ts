8ad21baf80b45b34e633f42396b327892c715674
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-19 19:23:41 -0300

    refactor(orders): renombrar relacion users → user en entidad Order, usar timestamptz

--- src/orders/entities/orders.entity.ts
+++ src/orders/entities/orders.entity.ts
@@ -1,22 +1,31 @@
 import { User } from "src/users/entities/users.entity";
-import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { OrderDetail } from "./orderDetails.entity";
 
 @Entity({
-    name: 'orders',
+  name: "orders",
 })
 export class Order {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({ type: "date"})
-    date: Date;
+  @Column({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+  date: Date;
 
-    @ManyToOne(() => User, (user) => user.orders)
-    @JoinColumn({ name: 'user_id'})
-    users: User;
+  @ManyToOne(() => User, (user) => user.orders, { nullable: false })
+  @JoinColumn({ name: "user_id" })
+  users: User;
 
-    @OneToOne(() => OrderDetail, (orderDetail) => orderDetail.order)
-    @JoinColumn({ name: 'order_detail_id'})
-    orderDetails: OrderDetail;
+  @OneToOne(() => OrderDetail, (orderDetail) => orderDetail.order, {
+    cascade: true,
+  })
+  @JoinColumn({ name: "order_detail_id" })
+  orderDetails: OrderDetail;
 }
