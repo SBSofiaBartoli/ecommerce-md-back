47f10195206dbc62cde79bbe478cc325022f1aee
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-22 17:20:01 -0300

    refactor(users/entity): consolidar campos, hacer country/city opcionales, limpiar nullable

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -3,46 +3,45 @@ import { Order } from "../../orders/entities/orders.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
-    name: 'users',
+  name: "users",
 })
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({ type: 'varchar', length: 50, nullable: false, })
-    name: string;
-    
-    @Column({ 
-        type: 'varchar',
-        length: 50,
-        nullable: false,
-        unique: true,
-    })
-    email: string;
+  @Column({ type: "varchar", length: 50, nullable: false })
+  name: string;
 
-    @Column({ type: 'varchar', length: 100, nullable: false })
-    password: string;
+  @Column({
+    type: "varchar",
+    length: 80,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
 
-    @Column('text')
-    address: string;
+  @Column({ type: "varchar", length: 100, nullable: false })
+  password: string;
 
-    @Column({ type: 'bigint' })
-    phone: number;
+  @Column({ type: "varchar", length: 100, nullable: false })
+  address: string;
 
-    @Column({ type: 'varchar', length: 50, })
-    country: string;
+  @Column({ type: "bigint", nullable: false })
+  phone: number;
 
-    @Column({ type: 'varchar', length: 50, })
-    city: string;
+  @Column({ type: "varchar", length: 50, nullable: true })
+  country: string;
 
-    @Column({
-        type: 'enum',
-        enum : Rol,
-        default: Rol.User,
-        nullable: true,
-    })
-    isAdmin: Rol;
+  @Column({ type: "varchar", length: 50 })
+  city: string;
 
-    @OneToMany(() => Order, (order) => order.users)
-    orders: Order[];
+  @Column({
+    type: "enum",
+    enum: Rol,
+    default: Rol.User,
+  })
+  isAdmin: Rol;
+
+  @OneToMany(() => Order, (order) => order.users)
+  orders: Order[];
 }
