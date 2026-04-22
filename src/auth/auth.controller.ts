880ec5d2740c54753d81ac73ba15d47f055bf072
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-22 19:16:19 -0300

    refactor(auth/controller): simplificar rutas a POST /signup y POST /signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,22 +2,24 @@ import { Body, Controller, Get, HttpCode, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "src/users/dtos/user.dto";
 import { LoginUserDto } from "src/users/dtos/loginUser.dto";
-import { ApiOperation } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
-@Controller('auth')
+@ApiTags("Auth")
+@Controller("auth")
 export class AuthController {
-    constructor (private readonly authService: AuthService) {}
-    
-    @ApiOperation({ summary: 'Create new user' })
-    @Post('signup')
-    @HttpCode(201)
-    createUserController(@Body() user: CreateUserDto) {
-        return this.authService.createUser(user);
-    }
+  constructor(private readonly authService: AuthService) {}
 
-    @ApiOperation({ summary: 'User login' })
-    @Post('/signin')
-    LoginUserController(@Body() credentials: LoginUserDto) {
-        return this.authService.loginUser(credentials);
-    }
+  @ApiOperation({ summary: "Register new user" })
+  @Post("signup")
+  @HttpCode(201)
+  createUserController(@Body() user: CreateUserDto) {
+    return this.authService.createUser(user);
+  }
+
+  @ApiOperation({ summary: "User login" })
+  @Post("/signin")
+  @HttpCode(200)
+  LoginUserController(@Body() credentials: LoginUserDto) {
+    return this.authService.loginUser(credentials);
+  }
 }
