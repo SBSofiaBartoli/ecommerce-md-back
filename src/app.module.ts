7f4dab5a6be59c51d1396c661adea5a0138032bc
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-22 19:20:05 -0300

    feat(app): configurar AppModule con JwtModule global, prefijo /api y middleware de logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { ProductsModule } from './products/products.module';
-import { AuthModule } from './auth/auth.module';
-import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { CategoryModule } from './categories/categories.module';
-import { OrderModule } from './orders/orders.module';
-import typeorm from './config/typeorm';
-import { FileUploadModule } from './file-upload/file-upload.module';
-import { JwtModule } from '@nestjs/jwt';
-import { SeederModule } from './seeder/seeder.module';
-
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { ProductsModule } from "./products/products.module";
+import { AuthModule } from "./auth/auth.module";
+import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { CategoryModule } from "./categories/categories.module";
+import { OrderModule } from "./orders/orders.module";
+import typeorm from "./config/typeorm";
+import { FileUploadModule } from "./file-upload/file-upload.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
@@ -21,18 +24,28 @@ import { SeederModule } from './seeder/seeder.module';
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => configService.get('typeorm') as TypeOrmModuleOptions,
+      useFactory: (configService: ConfigService) =>
+        configService.get("typeorm") as TypeOrmModuleOptions,
     }),
     JwtModule.register({
-    global: true,
-    signOptions: { expiresIn: '1h' },
-    secret: process.env.JWT_SECRET,
-    }), 
-    UsersModule, ProductsModule, AuthModule, CategoryModule, OrderModule, FileUploadModule, SeederModule ],
+      global: true,
+      signOptions: { expiresIn: "1d" },
+      secret: process.env.JWT_SECRET,
+    }),
+    UsersModule,
+    ProductsModule,
+    AuthModule,
+    CategoryModule,
+    OrderModule,
+    FileUploadModule,
+  ],
   controllers: [],
-  providers: []})
+  providers: [],
+})
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: "*", method: RequestMethod.ALL });
   }
 }
