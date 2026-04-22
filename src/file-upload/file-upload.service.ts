8456bcea816d39e79a07f3db59b3ffcfb14afc95
SofiaBartoli <sofiadbartoli@gmail.com>
2026-04-22 19:18:02 -0300

    refactor(file-upload/service): retornar producto con relaciones

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -6,18 +6,22 @@ import { Repository } from "typeorm";
 
 @Injectable()
 export class FileUploadService {
-    constructor (
-        private readonly fileUploadRepository: FileUploadRepository,
-        @InjectRepository(Product) private readonly productsRepository: Repository<Product>,
-    ) {}
+  constructor(
+    private readonly fileUploadRepository: FileUploadRepository,
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
 
-    async uploadImg(file: Express.Multer.File, productId: string) {
-        const product = await this.productsRepository.findOneBy({ id: productId });
-        if(!product) throw new NotFoundException('Product not found');
-        const upload = await this.fileUploadRepository.uploadImage(file);
-        await this.productsRepository.update(product.id, {
-            imgUrl: upload.secure_url,
-        })
-        return await this.productsRepository.findOneBy({ id: productId });
-    }
+  async uploadImg(file: Express.Multer.File, productId: string) {
+    const product = await this.productsRepository.findOneBy({ id: productId });
+    if (!product) throw new NotFoundException("Product not found");
+    const upload = await this.fileUploadRepository.uploadImage(file);
+    await this.productsRepository.update(product.id, {
+      imgUrl: upload.secure_url,
+    });
+    return await this.productsRepository.findOne({
+      where: { id: productId },
+      relations: { colors: true, category: true },
+    });
+  }
 }
